efe784e3ba278c4a91dce50dc9e928310c346627
Kyle Stewart <kylestew@gmail.com>
2026-01-05 16:15:09 +0000

    ...

--- python/chips/not_gate.py
+++ python/chips/not_gate.py
@@ -20,6 +20,5 @@ def not_gate(a: bool) -> bool:
     Allowed chips: nand
     Hint: What happens when you NAND a signal with itself?
     """
-    # DELETE THIS LINE AND IMPLEMENT:
+    # DELETE THIS LINE AND IMPLEMENT (don't forget RETURN!):
     raise NotImplementedError("not_gate")
-
